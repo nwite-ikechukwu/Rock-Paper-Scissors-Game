677249db29a037019a086b195c6d46e78b66bdb0
stanike1122 <ankersoundstore@gmail.com>
2023-08-12 11:04:06 +0100

    added restart game functionality

--- script.js
+++ script.js
@@ -1,30 +1,33 @@
-
 function generateComputerMove() {
   const randomNumber = Math.random();
-  let computerMove = '';
+  let computerMove = "";
 
-  if (randomNumber >=0 && randomNumber < 1/3 ) {
-    computerMove = 'rock';
-  }else if (randomNumber >= 1/3 && randomNumber < 2/3 ) {
-    computerMove = 'paper';
-  }else if (randomNumber >= 2/3 && randomNumber < 1 ) {
-    computerMove = 'scissors';
+  if (randomNumber >= 0 && randomNumber < 1 / 3) {
+    computerMove = "rock";
+  } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
+    computerMove = "paper";
+  } else if (randomNumber >= 2 / 3 && randomNumber < 1) {
+    computerMove = "scissors";
   }
   return computerMove;
 }
 
+const computerMove = generateComputerMove();
+let result = "";
+let score = JSON.parse(localStorage.getItem("score")) || {
+  you: 0,
+  comp: 0,
+};
+let playCount = 0;
+const playerComputerMoves = document.querySelector(".js-container");
+const playerMoveDiv = document.querySelector(".js-rock");
+const computerMoveDiv = document.querySelector(".js-scissors");
+const resultDiv = document.querySelector(".js-paper");
+const scoreBoard = document.querySelector(".js-score-board");
+let restartButton = document.querySelector('.js-restart');
+let resetButton = document.querySelector('.js-reset');
+
 function playGame(playerMove) {
-  const computerMove = generateComputerMove();
-  let result = '';
-  let score = {
-    you: 0,
-    comp: 0,
-  }
-  const playerComputerMoves = document.querySelector('.js-container')
-  const playerMoveDiv = document.querySelector('.js-rock');
-  const computerMoveDiv = document.querySelector('.js-scissors');
-  const resultDiv = document.querySelector('.js-paper');
-  const scoreBoard = document.querySelector('.js-score-board');
 
   function displayResult() {
     playerComputerMoves.innerHTML = `<div class="move-button">
@@ -39,76 +42,82 @@ function playGame(playerMove) {
     <img src="images/${computerMove}.png" class="move-icon no-pointer">
     <img class="shadow" src="images/shadow.png">
     <p>Com</p>
-    </div>`
+    </div>`;
   }
 
-
-  if (playerMove === 'rock') {
-    if (computerMove === 'rock') {
-      result = 'Tie';
+  if (playerMove === "rock") {
+    if (computerMove === "rock") {
+      result = "Tie";
       displayResult();
       updateScore();
       showScore();
-    }else if (computerMove === 'paper') {
-      result = 'You lose';
+    } else if (computerMove === "paper") {
+      result = "You lose";
       displayResult();
       updateScore();
       showScore();
-    }else if (computerMove === 'scissors') {
-      result = 'You win';
-      displayResult();
-      updateScore();
-      showScore();
-    }
-  }else if (playerMove === 'paper') {
-    if (computerMove === 'rock') {
-      result = 'You win';
-      displayResult();
-      updateScore();
-      showScore();
-    }else if (computerMove === 'paper') {
-      result = 'Tie';
-      displayResult();
-      updateScore();
-      showScore();
-    }else if (computerMove === 'scissors') {
-      result = 'You lose';
+    } else if (computerMove === "scissors") {
+      result = "You win";
       displayResult();
       updateScore();
       showScore();
     }
-  }else if (playerMove === 'scissors') {
-    if (computerMove === 'rock') {
-      result = 'You lose';
+  } else if (playerMove === "paper") {
+    if (computerMove === "rock") {
+      result = "You win";
       displayResult();
       updateScore();
       showScore();
-    }else if (computerMove === 'paper') {
-      result = 'You win';
+    } else if (computerMove === "paper") {
+      result = "Tie";
       displayResult();
       updateScore();
       showScore();
-    }else if (computerMove === 'scissors') {
-      result = 'Tie';
+    } else if (computerMove === "scissors") {
+      result = "You lose";
+      displayResult();
+      updateScore();
+      showScore();
+    }
+  } else if (playerMove === "scissors") {
+    if (computerMove === "rock") {
+      result = "You lose";
+      displayResult();
+      updateScore();
+      showScore();
+    } else if (computerMove === "paper") {
+      result = "You win";
+      displayResult();
+      updateScore();
+      showScore();
+    } else if (computerMove === "scissors") {
+      result = "Tie";
       displayResult();
       updateScore();
       showScore();
     }
   }
-
 
   function updateScore() {
-    if (result === 'You win') {
+    if (result === "You win") {
       score.you += 1;
-    }else if (result=== 'You lose') {
+    } else if (result === "You lose") {
       score.comp += 1;
     }
+    localStorage.setItem("score", JSON.stringify(score));
   }
-  scoreBoard.classList.add('score-board')
+
+  function resetScore() {
+    localStorage.removeItem('score');
+  }
+
+  scoreBoard.classList.add("score-board");
+  restartButton.classList.add("restart");
+  resetButton.classList.add("restart");
 
   function showScore() {
-    scoreBoard.innerHTML = `You ${score.you} : ${score.comp} Comp`
+    scoreBoard.innerHTML = `You ${score.you} : ${score.comp} Com`;
+    restartButton.innerHTML = 'Play again';
+    resetButton.innerHTML = 'Reset score';
   }
- 
 }
-
